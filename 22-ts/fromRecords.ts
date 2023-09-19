cae87e10607468f5a6d19b87c50846e1d0c538eb
zhangdi <1258956799@qq.com>
2023-09-19 19:34:40 +0800

    feat:添加动态解构对象参数

---
+++ 22-ts/fromRecords.ts
@@ -0,0 +1,20 @@
+function fromRecords<T>(items: T[], key: keyof T, value: keyof T) {
+  return Object.fromEntries(items.map(({[key]:k,[value]:v})=>{
+    return [k, v]
+  }));
+}
+
+type Person = {
+  id: string;
+  name: string;
+  age: number;
+};
+
+const people: Person[] = [
+  { id: '101', name: 'Alice', age: 28 },
+  { id: '102', name: 'Bob', age: 35 },
+  { id: '103', name: 'Charlie', age: 40 },
+];
+
+const nameById = fromRecords(people, 'id', 'name');
+console.log(nameById); 
